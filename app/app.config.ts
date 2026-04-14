e6b85079318c2b9558207819b35ff09a0bc0cab1
CyrilRXV <cyril.rohner@ynov.com>
2026-04-14 23:16:53 +0200

    feat(style): customise color

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'amber',
-      neutral: 'gray'
+      primary: 'primarySite',
+      secondary: 'secondarySite',
+      tertiary: 'tertiarySite'
     }
   }
 });
