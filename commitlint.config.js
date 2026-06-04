99659447904c9ab6811e5d628ebd400405bfba0e
Rmsaone <rmsaone@gmail.com>
2026-06-04 15:23:24 +0200

    feat(ci): add ci:local and commande script for test

---
+++ commitlint.config.js
@@ -0,0 +1,36 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    // ─── Type ───────────────────────────────
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'ci',
+        'chore',
+        'revert'
+      ]
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+
+    // ─── Scope ──────────────────────────────
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-enum': [0],
+
+    // ─── Subject ────────────────────────────
+    'subject-empty': [2, 'never'],
+    'subject-case': [1, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-min-length': [2, 'always', 3],
+    'subject-max-length': [2, 'always', 100]
+  }
+};
